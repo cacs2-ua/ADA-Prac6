392d38ca2447ac4de521b1c85e481db308704abe
cacs2-ua <cacs2@alu.ua.es>
2024-03-23 17:38:02 +0100

    Management of arguments done

---
+++ Code-ADA-Prac6/tests.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <limits>
+
+using namespace std;
+
+void showUsage() {
+    cout << "Usage:" << endl 
+         << "mcp [--p2D] [-t] [--ignore-naive] -f file" << endl;
+}
+
+void argumentsChecking(int argc, char* argv[], bool &t, 
+                        bool &p2D, bool &ignoreNaive,
+                        string &fileName) {
+    if (argc == 1 && string(argv[0]) != "mcp") {
+        showUsage();
+        exit(EXIT_SUCCESS);
+    }
+
+    for (unsigned int i = 1;  i < (unsigned int) argc; i++){
+        string arg = argv[i];
+        if(arg == "-t") {
+            t = true;
+        }
+        else if (arg == "--p2D") {
+            p2D = true;
+        }
+        else if (arg == "--ignore-naive") {
+            ignoreNaive = true;
+        }
+        else if (arg == "-f") {
+            i++;
+            if (i >= (unsigned int) argc) {
+                cerr << "ERROR: missing filename." << endl;
+                showUsage();
+                exit(EXIT_FAILURE);
+            }
+            fileName = argv[i];
+            if (fileName.empty()) {
+                cerr << "ERROR: missing filename." << endl;
+                showUsage();
+                exit(EXIT_FAILURE);
+            }
+            ifstream is(fileName);
+
+            if(!is) {
+                cerr << "ERROR: can’t open file: " + fileName + "." << endl ;
+                showUsage();
+                exit (EXIT_FAILURE);
+            }
+        }
+        else {
+            cerr << "ERROR: unknown option " + arg << endl;
+            showUsage();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+}
+
+int main (int argc, char* argv[]) {
+    bool t = false;
+    bool p2D = false;
+    bool ignoreNaive = false;
+    string fileName;
+    argumentsChecking(argc,argv,t,p2D,ignoreNaive,fileName);
+    cout << "Evereything Okay :D" << endl;
+    cout << "t: " << t << endl
+         << "p2D: " << p2D << endl
+         << "ignoreNaive: " << ignoreNaive << endl
+         << "fileName: " << fileName << endl;
+    return 0;
+}
+
