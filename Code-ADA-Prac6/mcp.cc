13935ad594b272f859c1baa931173b4c6bb213ad
cacs2-ua <cacs2@alu.ua.es>
2024-03-24 02:58:09 +0100

    memo-3.0

--- Code-ADA-Prac6/mcp.cc
+++ Code-ADA-Prac6/mcp.cc
@@ -7,7 +7,7 @@
 #include <iomanip>
 #include <sstream> 
 
-#define SENTINEL -1
+#define SENTINEL std::numeric_limits<size_t>::max()
 
 using namespace std;
 
@@ -138,34 +138,40 @@ size_t mcp_naive(const std::vector<std::vector<size_t>>& mcp, size_t n, size_t m
 }
 
 size_t mcp_memo(std::vector<std::vector<size_t>>& storage,const std::vector<std::vector<size_t>>& mcp, size_t n, size_t m, size_t i, size_t j) {
+    if (storage[n-1][m-1] != SENTINEL)
+        return storage[n-1][m-1];
+
     if (i == n-1) {
         for (size_t k = j; k < m; k++) {
             if (j == 0 && i != 0) { //if it's not the initial square
-                storage[n-1][k] = storage[n-2][k] + mcp[n-1][k];
+              return storage[n-1][k] = storage[n-2][k] + mcp[n-1][k];
             }
             else
-                storage[n-1][k] = storage[n-1][k-1] + mcp[n-1][k];
+              return storage[n-1][k] = storage[n-1][k-1] + mcp[n-1][k];
         }
-        return storage[n-1][m-1];
     }
+
     else if (j == m-1) {
         for (size_t k = i; k < n; k++) { 
             if (i == 0 && j != 0)   
-                storage[k][m-1] = storage[k][m-2] + mcp[k][m-1];
+              return storage[k][m-1] = storage[k][m-2] + mcp[k][m-1];
             else
-                storage[k][m-1] = storage[k-1][m-1] + mcp[k][m-1];
+               return storage[k][m-1] = storage[k-1][m-1] + mcp[k][m-1];
         }
-        return storage[n-1][m-1];
     }
+
     else {
-      storage[i][j] = min({
+     return storage[i][j] = min({
         mcp[i][j] + mcp_naive(mcp,n,m,i,j+1),
         mcp[i][j] + mcp_naive(mcp,n,m,i+1,j+1),
         mcp[i][j] + mcp_naive(mcp,n,m,i+1,j)
         });
-        return storage[n-1][m-1];
     }
+}
 
+size_t mcp_memo(const std::vector<std::vector<size_t>>& mcp, size_t n, size_t m, size_t i, size_t j) {
+    vector<vector<size_t>>storage(n, vector<size_t>(m, SENTINEL));
+    return mcp_memo(storage,mcp,n,m,i,j);
 }
 
 int main (int argc, char* argv[]) {
